04388fb839abdd94d757fc0a01b3427d2b914e7e
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-17 10:55:13 -0400

    Update if.js

--- m1_fundamentals/javascript/if.js
+++ m1_fundamentals/javascript/if.js
@@ -2,8 +2,14 @@
 // if(condition){
 // 	//code to be executed if condition is true
 // }
+// else{
+	//code to be executed if condition is false
+// }
 
-let num=5;
+let num=1;
 if(num>2){
 	console.log("number is greater than 2");
+}
+else{
+	console.log("number is less than 2");
 }
