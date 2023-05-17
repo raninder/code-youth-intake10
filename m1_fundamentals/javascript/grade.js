494e626394c27f456d8210cd2b9cf8f74bf78920
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-17 10:54:37 -0400

    multiple if-else

---
+++ m1_fundamentals/javascript/grade.js
@@ -0,0 +1,16 @@
+let grade=20;
+if(grade>80){
+	console.log("excellent");
+}
+else if(grade>70){
+	console.log("Very good");
+}
+else if(grade>60){
+	console.log("good");
+}
+else if(grade>50){
+	console.log("satisfied")
+}
+else{
+	console.log("poor");
+}
