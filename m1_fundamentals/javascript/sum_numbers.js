e801d821047bcc76d0d670eefded018967462028
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-18 10:51:40 -0400

    sum of numbers

---
+++ m1_fundamentals/javascript/sum_numbers.js
@@ -0,0 +1,12 @@
+let sum = 0;
+let num;
+for(let i=1;i<=5;i++){
+	num = Number(prompt("enter a number"));	
+	sum = sum + num;
+}
+console.log("sum:", sum);
+console.log("average",sum/5);
+
+// sum = 2+7+6+78+23
+
+// "2"+"3"   2+3
