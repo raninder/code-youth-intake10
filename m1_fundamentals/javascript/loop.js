492c66e8075b720575e77d05bfb0eaca779197d1
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-18 10:50:57 -0400

    loops introduction

---
+++ m1_fundamentals/javascript/loop.js
@@ -0,0 +1,16 @@
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+for(let i=1;i<=10;i+=2){
+	console.log(i);
+}
+
+
