8b1274178fe004a5e61dddf3054c8183eb5f45fe
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-23 10:51:12 -0400

    Create break-continue.js

---
+++ m1_fundamentals/javascript/break-continue.js
@@ -0,0 +1,14 @@
+console.log("break example");
+for(let i=0;i<10;i++){
+	if(i==5){
+		break;
+	}
+	console.log(i);
+}
+console.log("continue example");
+for(let i=0;i<10;i++){
+	if(i==5){
+		continue;
+	}
+	console.log(i);
+}
