6357d61237a0d4db75b4858c2ec7a0d3ce0aa2dc
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-23 10:51:35 -0400

    even-odd with functions

---
+++ m1_fundamentals/javascript/even-odd.js
@@ -0,0 +1,37 @@
+// function check(num){
+// 	if(num%2==0){
+// 		console.log("even");
+// 	}
+// 	else{
+// 		console.log("odd");
+// 	}
+// }
+
+// check(45);
+// check(12);
+
+//using return
+
+// function check(num){
+// 	if(num%2==0){
+// 		return "even";
+// 	}
+// 	else {
+// 		return "odd";
+// 	}
+// }
+
+// let number = check(23);
+// console.log(number)
+
+function checkEven(num){
+	if(num%2==0){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+let number = check(23);
+console.log(number)
