96078f04dd4573221daa337cbca5df4cd1cffeb1
Raninder Dhillon <raninderdhillon@gmail.com>
2023-05-26 10:51:49 -0400

    Update arrayMethods.js

--- m1_fundamentals/javascript/arrayMethods.js
+++ m1_fundamentals/javascript/arrayMethods.js
@@ -8,6 +8,7 @@ console.log("push-array",data);
 data.push(34,45);
 console.log("push-array",data);
 
+
 //pop()- to delete element from end of an array
  let ele = data.pop();
 console.log("pop-array",data);
@@ -49,3 +50,43 @@ console.log("spliced array",spliceArr);
 spliceArr = data.splice(2,1,222);
 console.log("original array", data);
 console.log("spliced array",spliceArr);
+
+
+//sort
+const stringArray = ['white','blue','green','grey','purple'];
+console.log(stringArray.sort());
+const data1 = [2,15,77,22,67];
+console.log("sorting",data1.sort());
+
+function arrSort(a,b){
+	return a-b;
+}
+
+console.log("sorting",data1.sort(arrSort));
+
+//reverse
+// for(i=data.length-1;i>=0;i--){
+// 	console.log(data1[i])
+// }
+
+console.log("reversing", data1.reverse());
+
+//join
+let joinArr = stringArray.join(' ') 
+console.log("join-",joinArr);
+let splitArr = joinArr.split(' ');
+console.log("split-", splitArr)
+
+
+
+const data2 = [2,15,77,22,67];
+let mapArray = data2.map((item)=> item+5);
+console.log("mapped", mapArray);
+ let filtredArray = data2.filter((element)=> element<25);
+ console.log("filtered", filtredArray);
+
+ let reduced = data2.reduce((sum,current)=>sum*current);
+ console.log("reduce", reduced);
+
+
+ console.log("array type", typeof data2);
